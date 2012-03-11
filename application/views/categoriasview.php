1e140f38eb3521613e1fe8a5db3542f2c7bf0123
Jose May <joslu.may@gmail.com>
2012-03-10 18:25:08 -0600

    adminus template

--- application/views/categoriasview.php
+++ application/views/categoriasview.php
@@ -1,106 +1,205 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>EMISOFT</title>
-		<link href="css/reset.css" type="text/css" rel="stylesheet"  />
-		<link href='http://fonts.googleapis.com/css?family=Molengo' rel='stylesheet' type='text/css'>
-		
-		<style type="text/css">
-		
-		table{
-	border: 1px solid black;
-	}
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-	table thead{
-	font-weight: bold;
-	}
-		
-		
-		</style>
+<head>
 
-	</head>
-	<body id="homeview">
-	
+	<meta http-equiv="X-UA-Compatible" content="IE=7" />
 
-	<!-- header -->
-	<div id="header">
-	<div class="wrapper"></div>
-	<h1 class="logo">EMISOFT</h1>
-	<h2 class="legend"></h2>
-	</div>
-	</div>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	
-	<!-- content -->
-	<div class="content wrapper">
+	<title>EMISOFT - Categor&iacute;as</title>
 	
-	<div class="hd">
-	
-	<form id="login" action="<?= site_url("CategoriasController/agregarCategoria"); ?>" method="post">
-	<label for="nombre">Nombre:</label>	<input type="text"  class="text" name="nombre"/>
-	<label for="descripcion">Descripci&oacute;n:</label>	<input type="text"  class="text" name="descripcion"/>
-	<label for="departamento">Departamento:</label>	<input type="text"  class="text" name="departamento_id"/>
-	
-	<input id="submit" type="submit" class="button confirm"  value="Entrar"/>
-	</form>
-	
-	</div>
-	
-	<div class="bd">
-	
-	
-                        <table>
-                            <thead>
-                                <tr>
-                                    <td>Nombre</td>
-                                    <td>Descripcion</td>
-                                    <td>Departamento</td>
-                                </tr>
-                            </thead>
+	<?= link_tag("css/style.css"); ?>
+    <?= link_tag("css/jquery.wysiwyg.css"); ?>
+    <?= link_tag("css/facebox.css"); ?>
+    <?= link_tag("css/visualize.css"); ?>
+    <?= link_tag("css/date_input.css"); ?>
 
-                            <tbody>
-                                <? foreach($items as $item): ?>
+	<!--[if lt IE 8]><style type="text/css" media="all">@import url("css/ie.css");</style><![endif]-->
+
+</head>
+
+<body>
+	
+	<div id="hld">
+	
+		<div class="wrapper">		<!-- wrapper begins -->
+	
+	
+			
+			<div id="header">
+				<div class="hdrl"></div>
+				<div class="hdrr"></div>
+				
+				<h1><a href="<?= site_url("/emisoft/CategoriasController"); ?>">EMISOFT</a></h1>
+				<?= $this->load->view("NavigationView"); ?>				
+				<p class="user">usuario activo: <?= $this->session->userdata("usuario"); ?>| <a href="<?= site_url("LoginController/cerrarSesion"); ?>">Salir</a></p>
+			</div>		<!-- #header ends -->
+			
+
+			<div class="block">
+			
+				<div class="block_head">
+					<div class="bheadl"></div>
+					<div class="bheadr"></div>
+					
+					<h2>Categor&iacute;as</h2>
+					
+					<ul>
+						
+					</ul>
+				</div>		<!-- .block_head ends -->
+				
+				
+				
+				<div class="block_content">
+				
+							<table cellpadding="0" cellspacing="0" width="100%" class="sortable">
+						
+							<thead>
+								<tr>
+							
+									<th>Nombre</th>
+                                    <th>Descripcion</td>
+                                    <th>Departamento</th>
+									<th>&nbsp;</th>
+								</tr>
+							</thead>
+							
+							<tbody>
+					
+					
+					<? foreach($items as $item): ?>
+								<? $detailsController =  site_url("CategoriasController/detallesCategoria/" . $item->id ); ?>
                                 <? $deleteController =  site_url("CategoriasController/eliminarCategoria/" . $item->id ); ?>
                                 <? $editController =  site_url( "CategoriasController/editarCategoria/" . $item->id ); ?>
 
                                 <tr>
-                                    <td><?= $item->nombre; ?></td>
+                                    <td><?= $item->nombre; ?>
+                                    </td>
 
-                                    <td><?= $item->descripcion; ?></td>
-                                  <td><?= $item->departamento_id; ?></td>
+                                    <td><?= $item->descripcion; ?>
+                                    </td>
+                                    
+                                    <td><?= $item->departamento_id; ?>
+                                    </td>
+                                    
                                   
-                                    <td class="tc"><a class="tooltip edit" 
-                                    href="<?= $editController; ?>" 
-                                    title="Editar Usuario">
-                                    <img src="<?= site_url('img/icons/small/user_edit.png'); ?>" alt="edit user" border="0" />
-                                    </a> <a class="tooltip delete" href="<?= $deleteController; ?>" title="Eliminar Usuario"><img src="<?= site_url('img/icons/small/user_delete.png'); ?>" alt="delete user" border="0" /></a></td>
-                                </tr><? endforeach; ?>
-                                
-                                
-                                </tbody>
-                        </table>
 
-                        <div class="table-options">
-                            <?= $linksPaginacion ?>
-                            <br class="cl" />
-                        </div>
+                                    <td class="tc">
+                                    
+                                    <a class="tooltip ver" 
+                                    href="<?= $detailsController; ?>" >
+                                    Detalles
+                                    </a>
+                                    |
+                                    <a class="tooltip edit" href="<?= $editController; ?>" >
+                                    Editar
+                                    </a> 
+                                    |
+                                    <a class="tooltip delete" 
+                                    href="<?= $deleteController; ?>">
+                                    Eliminar
+                                    </a>
+                                    
+                                    
+                                    </td>
+                 					</tr><? endforeach; ?>
 
-	
-	
-	
-	</div>
-	
-	
-	<div class="ft">
-	</div>
+								
+					
+								
+				
+							</tbody>
+							
+						</table>
+						
+						
+						
+												
 		
-	
-	</div>
-	
-	<!-- footer -->
-	<div id="footer">
-	<p>EMISOFT 2012</p>
-	</div>
-	
+						<div class="pagination right">
+						
+						
+						 <?= $linksPaginacion ?>
+						
+													
+													
+						</div>		<!-- .pagination ends -->
+										
+				</div>		<!-- .block_content ends -->
+				
+				<div class="bendl"></div>
+				
+				<div class="bendr"></div>
+			</div>		<!-- .block ends -->
+			
+			
 
-	</body>
+			<div class="block">
+			
+				<div class="block_head">
+					<div class="bheadl"></div>
+					<div class="bheadr"></div>
+					
+					<h2>Agregar Categor&iacute;a</h2>
+					
+				</div>		<!-- .block_head ends -->
+				
+				<div class="block_content">
+					<form action="<?= site_url("CategoriasController/agregarCategoria"); ?>" method="post">
+						<p>
+						<label for="nombre">Nombre:</label>
+						<input type="text"  class="text" name="nombre"/>
+						</p>
+						<p>
+						<label for="descripcion">Descripci&oacute;n:</label>	
+						<input type="text"  class="text" name="descripcion"/> 
+						</p>
+						<p>
+						<label for="departamento">Departamento:</label>	
+						<input type="text"  class="text" name="departamento_id"/>
+						</p>
+						
+						<input type="submit" class="submit small" value="Agregar" />
+						
+					</form>
+				</div>
+								
+				<div class="bendl"></div>
+				<div class="bendr"></div>
+					
+			</div>		<!-- .block ends -->
+		
+		
+			
+			<div id="footer">
+			
+				<p class="left"><a href="#">EMISOFT</a></p>
+				
+			</div>
+		
+		
+		</div>						<!-- wrapper ends -->
+		
+	</div>		<!-- #hld ends -->
+	
+	
+	<!--[if IE]><script type="text/javascript" src="js/excanvas.js"></script><![endif]-->	
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/jquery.img.preload.js"></script>
+	<script type="text/javascript" src="js/jquery.filestyle.mini.js"></script>
+	<script type="text/javascript" src="js/jquery.wysiwyg.js"></script>
+	<script type="text/javascript" src="js/jquery.date_input.pack.js"></script>
+	<script type="text/javascript" src="js/facebox.js"></script>
+	<script type="text/javascript" src="js/jquery.visualize.js"></script>
+	<script type="text/javascript" src="js/jquery.visualize.tooltip.js"></script>
+	<script type="text/javascript" src="js/jquery.select_skin.js"></script>
+	<script type="text/javascript" src="js/jquery.tablesorter.min.js"></script>
+	<script type="text/javascript" src="js/ajaxupload.js"></script>
+	<script type="text/javascript" src="js/jquery.pngfix.js"></script>
+	<script type="text/javascript" src="js/custom.js"></script>
+
+	
+</body>
 </html>
--- application/views/loginview.php
+++ application/views/loginview.php
@@ -1,47 +1,120 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>EMISOFT</title>
-		<link href="css/reset.css" type="text/css" rel="stylesheet"  />
-		<link href='http://fonts.googleapis.com/css?family=Molengo' rel='stylesheet' type='text/css'>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-	</head>
-	<body id="loginview">
-	
+<head>
 
-	<!-- header -->
-	<div id="header">
-	<div class="wrapper"></div>
-	<h1 class="logo">EMISOFT</h1>
-	<h2 class="legend"></h2>
-	</div>
-	</div>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	
-	<!-- content -->
-	<div class="content wrapper">
+	<title>EMISOFT - Acceso</title>
 	
-	<div id="login" class="panel">
-	<h2>Iniciar sesi&oacute;n</h2>
-	<div class="bd">
-	<form id="login" action="<?= site_url("LoginController/iniciarSesion"); ?>" method="post">
-	<label for="username">Usuario:</label>	<input type="text"  class="text" name="usuario"/>
-	<label for="password">Contrase&ntilde;a:</label>	<input type="password"  class="text" name="contrasena"/>
-	<input id="submit" type="submit" class="button confirm"  value="Entrar"/>
-	</form>
-	</div>
-	<div class="ft">
-	<a href="#">&iquest;Olvid&oacute; su contrase&ntilde;a?</a>
-	</div>
-	</div>
+  <?= link_tag("css/style.css"); ?>
+    <?= link_tag("css/jquery.wysiwyg.css"); ?>
+    <?= link_tag("css/facebox.css"); ?>
+    <?= link_tag("css/visualize.css"); ?>
+    <?= link_tag("css/date_input.css"); ?>
 	
-	
-	</div>
-	
-	<!-- footer -->
-	<div id="footer">
-	<p>EMISOFT 2012</p>
-	</div>
-	
+	<!--[if lt IE 8]><style type="text/css" media="all">@import url("css/ie.css");</style><![endif]-->
 
-	</body>
+</head>
+
+
+
+
+<body>
+	
+	<div id="hld">
+	
+		<div class="wrapper">		<!-- wrapper begins -->
+		
+		
+		
+		
+		
+		
+			
+			
+			
+			
+			
+			
+			
+			<div class="block small center login">
+			
+				<div class="block_head">
+					<div class="bheadl"></div>
+					<div class="bheadr"></div>
+					<h2>EMISOFT</h2>
+					<ul>
+					<li></li>
+					</ul>
+				
+				</div>		<!-- .block_head ends -->
+				
+				
+				
+				
+				<div class="block_content">
+					
+					<div class="message info"><p>Ingrese su nombre de usuario y contrase&ntilde;a</p></div>
+					
+					<form action="<?= site_url("LoginController/iniciarSesion"); ?>" method="post">
+						<p>
+							<label>Usuario:</label> <br />
+							<input type="text" class="text" value="" name="usuario" />
+						</p>
+						
+						<p>
+							<label>Contrasena:</label> <br />
+							<input type="password" class="text" value=""  name="contrasena"/>
+						</p>
+						
+						<p>
+							<input type="submit" class="submit" value="Entrar" /> &nbsp; 
+				
+						</p>
+					</form>
+					
+				</div>		<!-- .block_content ends -->
+					
+				<div class="bendl"></div>
+				<div class="bendr"></div>
+								
+			</div>		<!-- .login ends -->
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		
+		
+		</div>						<!-- wrapper ends -->
+		
+	</div>		<!-- #hld ends -->
+	
+	
+	<!--[if IE]><script type="text/javascript" src="js/excanvas.js"></script><![endif]-->	
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/jquery.img.preload.js"></script>
+	<script type="text/javascript" src="js/jquery.filestyle.mini.js"></script>
+	<script type="text/javascript" src="js/jquery.wysiwyg.js"></script>
+	<script type="text/javascript" src="js/jquery.date_input.pack.js"></script>
+	<script type="text/javascript" src="js/facebox.js"></script>
+	<script type="text/javascript" src="js/jquery.visualize.js"></script>
+	<script type="text/javascript" src="js/jquery.visualize.tooltip.js"></script>
+	<script type="text/javascript" src="js/jquery.select_skin.js"></script>
+	<script type="text/javascript" src="js/jquery.tablesorter.min.js"></script>
+	<script type="text/javascript" src="js/ajaxupload.js"></script>
+	<script type="text/javascript" src="js/jquery.pngfix.js"></script>
+	<script type="text/javascript" src="js/custom.js"></script>
+	
+	
+</body>
 </html>
+
+
